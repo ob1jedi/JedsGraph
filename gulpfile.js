0a9a7b6310d34da3c449a61925a7237b096fe1ff
ob1jedi <jedqkay@googlemail.com>
2017-10-22 14:50:49 +0200

    Added gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var browserSync = require('browser-sync').create();
-
+var inject = require('gulp-inject');
 
 gulp.task('default', function() {
 	gulp.start('serve');
+});
+
+gulp.task('index', function () {
+  var target = gulp.src('./src/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['./src/js/**/*.js'], {read: false});
+ 
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./src'));
 });
 
 gulp.task('serve', function() {
