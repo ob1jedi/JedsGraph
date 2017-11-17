13c85b3b9cd7085eca5aa288e5251aeff2c6a256
ob1jedi <jedqkay@googlemail.com>
2017-11-17 22:01:46 +0200

    Attempting HTML migrate from vue js files to index.html

---
+++ src/scripts/VueComponents/ConsoleElement2.vue
@@ -0,0 +1,22 @@
+﻿<template>
+
+
+</template>
+
+<script>
+    
+    export default {
+    data() {
+      return {
+        checked: false,
+        title: 'Check me'
+      }
+    },
+    methods: {
+      check() {
+        this.checked = !this.checked;
+      }
+    }
+  }
+
+</script>
